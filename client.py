d61f04b4666197c5de58343b8a05d2fea2996dca
Jack Hermanson <jack.hermanson@live.com>
2025-01-25 23:29:10 -0700

    minor improvements

--- client.py
+++ client.py
@@ -4,25 +4,28 @@ from random import random, randint
 import requests
 import datetime
 
-BASE_URL = "http://localhost:5040"
+BASE_URL = "http://localhost:5040"  # URL for the server.
+
+# Send a POST request to the server.
 req = requests.post(
     url=f"{BASE_URL}/accept-payment",
     data={
         "timestamp": datetime.datetime.now().isoformat(),
-        "card_number": "-".join(f"{randint(0, 9999):04}" for _ in range(4)),
-        "card_security_code": f"{randint(1, 999):03}",
-        "zip_code": f"80{randint(1, 999):03}",
-        "amount": randint(50, 1000) + round(random(), 2),
+        "card_number": "-".join(f"{randint(0, 9999):04}" for _ in range(4)),  # Randomized card number.
+        "card_security_code": f"{randint(1, 999):03}",  # Randomized security code.
+        "zip_code": f"80{randint(1, 999):03}",  # Randomized zip code.
+        "amount": randint(50, 1000) + round(random(), 2),  # Randomized transaction amount.
         "name": "Jackson Harrison",
         "email": "jackson@gmail.com",
     },
     headers={
         "Content-Type": "application/x-www-form-urlencoded",
-        # "Accept": "text/plain",
+        # "Accept": "text/plain",  # This would cause a 406 error.
         "Accept": "application/json",
     }
 )
 
+# Get the response back as json and print it out.
 response = req.json()
 print(json.dumps(response, indent=2))
 
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Week 4 Assignment 2
+
+## Summary
+
+There are two main scripts. One, `server.py`, listens for authorization requests via HTTP on localhost port 5040.
+The other, `client.py`, sends requests. The body of each request is randomized, but fits a certain shape.
+
+
